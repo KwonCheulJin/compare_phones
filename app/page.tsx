d1aa3dd1a534c5fcb261b583430165ef2fdd53c5
KwonCheulJin <chkftm12@gmail.com>
2025-02-11 16:03:50 +0900

    supabase gen types and supabase 연동 작업

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
+import { createClient } from '@/utils/supabase/server';
+
+export default async function Home() {
+  const supabase = await createClient();
+  const { data } = await supabase.from('phones').select('*, phone_colors(*)');
+  console.log('🚀 ~ Home ~ data:', data);
   return <div>Hello</div>;
 }
